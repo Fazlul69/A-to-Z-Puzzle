e0d92719e745b7f95c445e9fc0ef74ecb3b07043
Fazlul Hoque <sawravm299@gmail.com>
2020-06-04 19:14:35 +0600

    Initial commit

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject SelectedPiece;
+    int OIL = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition),Vector2.zero);
+            if (hit.transform.CompareTag("puzzle")) {
+
+                if(!hit.transform.GetComponent<PiecesScript>().InRightPosition)
+                {
+                    SelectedPiece = hit.transform.gameObject;
+                    SelectedPiece.GetComponent<PiecesScript>().Selected = true;
+                    SelectedPiece.GetComponent<SortingGroup>().sortingOrder = OIL;
+                    OIL++;
+                }
+                
+            }
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            if (SelectedPiece != null)
+            {
+                SelectedPiece.GetComponent<PiecesScript>().Selected = false;
+                SelectedPiece = null;
+            }
+            
+        }
+        if (SelectedPiece != null) {
+            Vector3 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            SelectedPiece.transform.position = new Vector3(mousePoint.x, mousePoint.y, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PiecesScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PiecesScript : MonoBehaviour
+{
+    private Vector3 rightPosition;
+    public bool InRightPosition;
+    public bool Selected;
+    void Start()
+    {
+        rightPosition = transform.position;
+        transform.position = new Vector3(Random.Range(1f, 9f), Random.Range(4f, -4.5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, rightPosition) < 0.5f)
+        {
+            if(!Selected)
+            {
+                if (InRightPosition == false)
+                {
+                    transform.position = rightPosition;
+                    InRightPosition = true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+                }              
+            }
+        }
+    }
+}
